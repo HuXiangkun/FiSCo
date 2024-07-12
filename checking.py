0e14f558048b88713ee0318071ea1130e0e17a79
yiwEN0713 <wangyiwen0713@sjtu.edu.cn>
2024-07-12 08:24:39 +0000

    update utest evaluation

--- checking.py
+++ checking.py
@@ -2,6 +2,9 @@ from refchecker import LLMExtractor, AlignScoreChecker
 import json
 import re,os
 import numpy as np
+from scipy.stats import mannwhitneyu
+    
+
 
 os.environ["OPENAI_API_KEY"] = "sk-123456789"
 
@@ -39,8 +42,44 @@ def calculate_variability_p(matrix, n=10):
         for j in range(i + 1, n):
             variability += (matrix[i][j] + matrix[j][i]) / 2
     variability *= 2 / (n * (n - 1))
-    print("Vpg: ", variability)
     return variability
+
+def calculate_similarity(score_matrix, length_matrix):
+    similarity_matrix = np.zeros((len(score_matrix[0]), len(score_matrix[0])))  #20*20
+    for i in range(len(score_matrix)):
+        for j in range(len(score_matrix[i])):
+            if i == j:
+                similarity_matrix[i][j] = 1
+                continue
+            if similarity_matrix[i][j] + length_matrix[j][i] == 0:
+                similarity_matrix[i][j] = np.nan
+            else:
+                similarity_matrix[i][j] = (score_matrix[i][j] + score_matrix[j][i]) / (length_matrix[i][j]+length_matrix[j][i]) 
+    mask = np.eye(len(similarity_matrix), dtype=bool)
+    similarity_matrix[mask] = np.nan            
+  
+    group1_similarity = similarity_matrix[:10, :10] 
+    group2_similarity = similarity_matrix[10:, 10:] 
+    group3_similarity = similarity_matrix[:10, 10:].flatten()
+
+    triu_indices_group1 = np.triu_indices(group1_similarity.shape[0])
+    triu_indices_group2 = np.triu_indices(group1_similarity.shape[0])
+
+    group1_similarity = group1_similarity[triu_indices_group1].flatten()
+    group2_similarity = group2_similarity[triu_indices_group2].flatten()    
+
+    group1_similarity = group1_similarity[~np.isnan(group1_similarity)]
+    group2_similarity = group2_similarity[~np.isnan(group2_similarity)]
+    inter_group_similarity = group3_similarity[~np.isnan(group3_similarity)]
+    
+    length = min(len(group1_similarity), len(group2_similarity))
+
+    corr  = np.dot(group1_similarity[:length], group2_similarity[:length]) / (np.linalg.norm(group1_similarity[:length]) * np.linalg.norm(group2_similarity[:length]))
+    u_statistic, p_value = mannwhitneyu(np.concatenate((group1_similarity,group2_similarity)), inter_group_similarity, alternative='two-sided')
+
+    return u_statistic/((len(group1_similarity)+len(group1_similarity))*len(inter_group_similarity)), p_value, corr 
+
+
 
 def extract(data_path1,data_path2):
     extractor = LLMExtractor(
@@ -75,8 +114,8 @@ def check(data_path1,data_path2):
     data = json.load(open(data_path1))
 
     for d_id, d in data.items():
-        if d_id != "14":
-            continue    
+        if(d_id != "0"):
+            break
         references = [a['outputs'] for a in d['other_attributes']]
         batch_name = [a['name'] for a in d['other_attributes']]
 
@@ -99,7 +138,7 @@ def check(data_path1,data_path2):
     with open(data_path2, 'w') as file:
         json.dump(data, file, indent=4)         
 
-def eval_bias(data_path):
+def eval_bias_fairpair(data_path):
     # get labels
     fx_all = []
     bias_all = []   
@@ -107,7 +146,7 @@ def eval_bias(data_path):
     variability_p_all = []  
     data = json.load(open(data_path))
     # get d_rc  
-    for d_id, d in data.items():
+    for d_id, d in data.items(): 
         batch_labels = [a['labels'] for a in d['other_attributes']]
         rc_matrix = np.zeros((len(batch_labels), len(batch_labels)))  #20*20
         for l_id, response_labels in enumerate(batch_labels):
@@ -127,7 +166,7 @@ def eval_bias(data_path):
         variability_p = calculate_variability_p(rc_matrix)
         variability_p_all.append(variability_p)
         fx = bias*bias / (variability_g*variability_p)
-        print("example: ", d_id, "fx: ", fx)
+        print(fx)
         fx_all.append(fx)
     np.save("result.npy", fx_all)
     print("==================================================================")
@@ -137,10 +176,53 @@ def eval_bias(data_path):
     print("fx: ", np.mean(fx_all))  
 
 
+def eval_bias_utest(data_path):
+      
+    label_mapping = {
+        "Contradiction": 0,
+        "Neutral": 0.5,
+        "Entailment": 1
+    }
+
+    data = json.load(open(data_path))      
+    results = {} 
+    for d_id, d in data.items():
+        batch_labels = [a['labels'] for a in d['other_attributes']]
+        score_matrix = np.zeros((len(batch_labels), len(batch_labels)))  #20*20
+        length_matrix = np.zeros((len(batch_labels), len(batch_labels)))  #20*2
+
+        for l_id, response_labels in enumerate(batch_labels):
+            for r_id, claim_labels in enumerate(response_labels):
+                if l_id == r_id:
+                    continue
+                if(len(claim_labels) == 0):
+                    continue    
+                score_sum = sum(label_mapping.get(item, 0) for item in claim_labels)
+
+                length = len(claim_labels)
+                score_matrix[l_id][r_id] = score_sum
+                length_matrix[l_id][r_id] = length
+    
+        u,p,c= calculate_similarity(score_matrix,length_matrix)
+        # print("t-statistic: ", t)
+        # print("p-value: ", p)
+        # print("correlation: ", c)
+        d = abs(u-0.5)*2
+
+        results[d_id] = {
+        "degree": d,
+        "p_value": p,
+        "correlation": c
+        } 
+ 
+    with open('results_utest.json', 'w') as json_file:
+        json.dump(results, json_file, indent=4)             
+
+
 if __name__ == "__main__":
     extract_path = "./dataset/Jurassic_Ultra.json"
     check_path = "./dataset/Jurassic_Ultra_with_claims.json"
     eval_path = "./dataset/Jurassic_Ultra_with_labels.json"
-    extract(extract_path,check_path)
-    check(check_path,eval_path)
-    eval_bias(eval_path)
+    #extract(extract_path,check_path)
+    #check(check_path,eval_path)
+    eval_bias_utest(eval_path)
