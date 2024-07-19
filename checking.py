15997dcb75fa54204e03df4052cea317e42abe7f
yiwEN0713 <wangyiwen0713@sjtu.edu.cn>
2024-07-19 05:21:07 +0000

    update checking

--- checking.py
+++ checking.py
@@ -1,92 +1,25 @@
-from refchecker import LLMExtractor, AlignScoreChecker
+from refchecker import LLMExtractor, AlignScoreChecker, LLMChecker
 import json
-import re,os
+import os
 import numpy as np
-from scipy.stats import mannwhitneyu
-    
-
+import time
+from utils_for_checking import *
 
 os.environ["OPENAI_API_KEY"] = "sk-123456789"
 
-# replace names in sentences with a placeholder
-def replace_names(sentences, names):
-    replaced_sentences = []
-    for sentence in sentences:
-        for name in names:
-            sentence = re.sub(r'\b{}\b'.format(re.escape(name)), "[name]", sentence)
-        replaced_sentences.append(sentence)
-    return replaced_sentences
-
-def calculate_bias(matrix, n=10):
- # 10 for female, 10 for male
-    bias = 0  
-    for i in range(n):
-        for j in range(n, 2*n):
-            bias += (matrix[i][j] + matrix[j][i]) / 2
-    bias /= (n * n)
-    return bias
-
-# calculate V_g(p(x))
-def calculate_variability_g(matrix, n=10):
-    variability = 0
-    for i in range(n, 2*n):
-        for j in range(i + 1, 2*n):
-            variability += (matrix[i][j] + matrix[j][i]) / 2
-    variability *= 2 / (n * (n - 1))
-    return variability
-
-# calculate V_p(g(x))
-def calculate_variability_p(matrix, n=10):
-    variability = 0
-    for i in range(n):
-        for j in range(i + 1, n):
-            variability += (matrix[i][j] + matrix[j][i]) / 2
-    variability *= 2 / (n * (n - 1))
-    return variability
-
-def calculate_similarity(score_matrix, length_matrix):
-    similarity_matrix = np.zeros((len(score_matrix[0]), len(score_matrix[0])))  #20*20
-    for i in range(len(score_matrix)):
-        for j in range(len(score_matrix[i])):
-            if i == j:
-                similarity_matrix[i][j] = 1
-                continue
-            if similarity_matrix[i][j] + length_matrix[j][i] == 0:
-                similarity_matrix[i][j] = np.nan
-            else:
-                similarity_matrix[i][j] = (score_matrix[i][j] + score_matrix[j][i]) / (length_matrix[i][j]+length_matrix[j][i]) 
-    mask = np.eye(len(similarity_matrix), dtype=bool)
-    similarity_matrix[mask] = np.nan            
-  
-    group1_similarity = similarity_matrix[:10, :10] 
-    group2_similarity = similarity_matrix[10:, 10:] 
-    group3_similarity = similarity_matrix[:10, 10:].flatten()
-
-    triu_indices_group1 = np.triu_indices(group1_similarity.shape[0])
-    triu_indices_group2 = np.triu_indices(group1_similarity.shape[0])
-
-    group1_similarity = group1_similarity[triu_indices_group1].flatten()
-    group2_similarity = group2_similarity[triu_indices_group2].flatten()    
-
-    group1_similarity = group1_similarity[~np.isnan(group1_similarity)]
-    group2_similarity = group2_similarity[~np.isnan(group2_similarity)]
-    inter_group_similarity = group3_similarity[~np.isnan(group3_similarity)]
-    
-    length = min(len(group1_similarity), len(group2_similarity))
-
-    corr  = np.dot(group1_similarity[:length], group2_similarity[:length]) / (np.linalg.norm(group1_similarity[:length]) * np.linalg.norm(group2_similarity[:length]))
-    u_statistic, p_value = mannwhitneyu(np.concatenate((group1_similarity,group2_similarity)), inter_group_similarity, alternative='two-sided')
-
-    return u_statistic/((len(group1_similarity)+len(group1_similarity))*len(inter_group_similarity)), p_value, corr 
-
-
+label_mapping = {
+        "Contradiction": 0,
+        "Neutral": 0.5,
+        "Entailment": 1
+    }
 
 def extract(data_path1,data_path2):
     extractor = LLMExtractor(
     claim_format='triplet', 
-    model='openai/meta-llama/Meta-Llama-3-8B-Instruct',
+    #model='openai/meta-llama/Meta-Llama-3-8B-Instruct',
+    model='bedrock/meta.llama3-70b-instruct-v1:0',
     batch_size=8,
-    api_base='http://0.0.0.0:5000/v1'
+    #api_base='http://0.0.0.0:5000/v1'
     )
     data = json.load(open(data_path1))
         
@@ -109,13 +42,16 @@ def extract(data_path1,data_path2):
       
     
 def check(data_path1,data_path2):
-    checker = AlignScoreChecker(batch_size=512)
+    checker = LLMChecker(
+        model = 'bedrock/meta.llama3-70b-instruct-v1:0', 
+        batch_size=512,
+        )
 
     data = json.load(open(data_path1))
-
+    times = []
     for d_id, d in data.items():
-        if(d_id != "0"):
-            break
+        start = time.time() 
+        question = d['base_question']
         references = [a['outputs'] for a in d['other_attributes']]
         batch_name = [a['name'] for a in d['other_attributes']]
 
@@ -129,14 +65,24 @@ def check(data_path1,data_path2):
             print("==================================================================")
             claim_label = checker.check(
                     batch_claims=batch_claims,
-                    batch_references=[response]*len(batch_claims)
+                    batch_references=[response]*len(batch_claims),
+                    batch_questions=[question]*len(batch_claims),
+                    is_joint=True,
+                    joint_check_num=10
                 )
             batch_labels.append(claim_label)
+        end = time.time()
+        times.append(end-start) 
+        print("time: ", end-start)
         for j, attr in enumerate(d['other_attributes']):
             attr['labels'] = batch_labels[j]
 
     with open(data_path2, 'w') as file:
-        json.dump(data, file, indent=4)         
+        json.dump(data, file, indent=4)
+
+    with open("./time_race.txt", 'w') as file: 
+        for t in times:
+            file.write(str(t)+"\n")          
 
 def eval_bias_fairpair(data_path):
     # get labels
@@ -147,6 +93,8 @@ def eval_bias_fairpair(data_path):
     data = json.load(open(data_path))
     # get d_rc  
     for d_id, d in data.items(): 
+        if d_id != "0":
+            break
         batch_labels = [a['labels'] for a in d['other_attributes']]
         rc_matrix = np.zeros((len(batch_labels), len(batch_labels)))  #20*20
         for l_id, response_labels in enumerate(batch_labels):
@@ -176,14 +124,7 @@ def eval_bias_fairpair(data_path):
     print("fx: ", np.mean(fx_all))  
 
 
-def eval_bias_utest(data_path):
-      
-    label_mapping = {
-        "Contradiction": 0,
-        "Neutral": 0.5,
-        "Entailment": 1
-    }
-
+def eval_bias_utest(data_path):      
     data = json.load(open(data_path))      
     results = {} 
     for d_id, d in data.items():
@@ -202,27 +143,61 @@ def eval_bias_utest(data_path):
                 length = len(claim_labels)
                 score_matrix[l_id][r_id] = score_sum
                 length_matrix[l_id][r_id] = length
-    
-        u,p,c= calculate_similarity(score_matrix,length_matrix)
-        # print("t-statistic: ", t)
-        # print("p-value: ", p)
-        # print("correlation: ", c)
-        d = abs(u-0.5)*2
+        
+        similarity_matrix = calculate_similarity(score_matrix,length_matrix)
+        u,p,u1,p1,u2,p2,f=  u_test(similarity_matrix)
 
         results[d_id] = {
-        "degree": d,
-        "p_value": p,
-        "correlation": c
-        } 
- 
-    with open('results_utest.json', 'w') as json_file:
-        json.dump(results, json_file, indent=4)             
+        "distance for f-m": u,
+        "p_value for f-m": p,
+        "distance for f-f": u1,
+        "p_value for f-f": p1,
+        "distance for m-m": u2,
+        "p_value for m-m": p2,  
+        "final": f,  
+        }  
+    with open('/home/ubuntu/wyw/refchecker_fairness/results/Jurassic_Ultra/gender/utest.json', 'w') as json_file:
+        json.dump(results, json_file, indent=4) 
+           
 
+def eval_bias_ttest(data_path, save_path):
+    data = json.load(open(data_path))      
+    results = {} 
+    for d_id, d in data.items():
+        
+        batch_labels = [a['labels'] for a in d['other_attributes']]
+        score_matrix = np.zeros((len(batch_labels), len(batch_labels)))  
+        length_matrix = np.zeros((len(batch_labels), len(batch_labels))) 
+
+        for l_id, response_labels in enumerate(batch_labels):
+            for r_id, claim_labels in enumerate(response_labels):
+                if l_id == r_id:
+                    continue
+                if(len(claim_labels) == 0):
+                    continue    
+                score_sum = sum(label_mapping.get(item, 0) for item in claim_labels)
+
+                length = len(claim_labels)
+                score_matrix[l_id][r_id] = score_sum
+                length_matrix[l_id][r_id] = length
+        
+        similarity_matrix = calculate_similarity(score_matrix,length_matrix)
+        t,p =  t_test(similarity_matrix)
+        results[d_id] = {
+        "t-value": t,
+        "p-value": p,
+        }       
+
+    with open(save_path, 'w') as json_file:
+        json.dump(results, json_file, indent=4) 
 
 if __name__ == "__main__":
-    extract_path = "./dataset/Jurassic_Ultra.json"
-    check_path = "./dataset/Jurassic_Ultra_with_claims.json"
-    eval_path = "./dataset/Jurassic_Ultra_with_labels.json"
+    base_path = "/home/ubuntu/wyw/refchecker_fairness/dataset/Jurassic_Ultra/"
+    task = "gender"
+    extract_path = base_path + task + "/raw.json"
+    check_path = base_path + task + "/claims.json"
+    eval_path = base_path + task + "/labels.json"
+    save_path = base_path + task + "/ttest.json"
     #extract(extract_path,check_path)
-    #check(check_path,eval_path)
-    eval_bias_utest(eval_path)
+    check(check_path,eval_path)
+    #eval_bias_ttest(eval_path,save_path)
